643067cc181e24f617f68b650edcbb94dc8d0750
Baby Leezus <118462761+babyleezus@users.noreply.github.com>
2023-12-12 23:45:40 +0000

    added basic html framework

---
+++ script.js
@@ -0,0 +1,14 @@
+let choices = ["rock", "paper", "scissors"]
+
+userChoice =
+
+computerChoice = r,p,s
+
+console.log("Enter your choice" )
+
+if userChoice === computerChoice
+console log(0)
+
+if userChoice = r and computerChoice = s 
+console.log ("You lose! User 0, Computer 1")
+if userChoice = p 
